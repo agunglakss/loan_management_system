28a85bf4f76178abdb77650849a3e09fa5222b13
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-11-16 14:25:24 +0800

    feat: add pickup service

---
+++ backend/app/services/loans/pickup_service.rb
@@ -0,0 +1,29 @@
+module Loans
+  class PickupService
+    def self.call(loan:)
+      Loan.transaction do
+        loan.with_lock do
+          raise ActiveRecord::RecordInvalid.new(loan), "Loan is not reserved" unless loan.status == "reserved"
+
+          actual_pickup = Time.current
+          due_at = loan.due_at
+          borrowed_at = loan.borrowed_at
+
+          if actual_pickup < borrowed_at
+            time_shift = actual_pickup - borrowed_at
+            due_at = due_at + time_shift
+            borrowed_at = actual_pickup
+          end
+
+          loan.update!(
+            status: "borrowed",
+            borrowed_at: borrowed_at,
+            due_at: due_at
+          )
+
+          loan
+        end
+      end
+    end
+  end
+end
