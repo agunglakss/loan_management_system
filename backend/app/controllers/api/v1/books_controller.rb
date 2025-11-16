a90c025eb0965600c175d950d114c0f7493f3416
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-11-16 14:22:03 +0800

    feat: add book controller

---
+++ backend/app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,60 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+    books = Book.includes(:author, :publisher)
+    render json: books.as_json(
+      include: {
+        author: { only: [:id, :name] },
+        publisher: { only: [:id, :name] }
+      },
+      methods: :available_count
+    )
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: book.as_json(
+      include: {
+        author: { only: [:id, :name] },
+        publisher: { only: [:id, :name] }
+      },
+      methods: :available_count
+    )
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book, status: :created
+    else
+      render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      render json: book
+    else
+      render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      render json: { message: "Book deleted" }
+    else
+      render json: { errors: book.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(
+      :title, :isbn,
+      :author_id, :publisher_id,
+      :total_books, :total_borrow
+    )
+  end
+end
