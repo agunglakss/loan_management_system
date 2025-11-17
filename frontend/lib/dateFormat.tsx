25b05464083cb5ed17aec6b4cddf2aaeb6b363ea
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-11-17 15:14:38 +0800

    feat: create new funtion for date formating

---
+++ frontend/lib/dateFormat.tsx
@@ -0,0 +1,8 @@
+export default function formatDate(dateString: string) {
+  return new Date(dateString).toLocaleDateString('id-ID', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+}
+
